8fa0d1ebfee5f5c5738e852572adc25cbc547887
mallory-scotton <mallory.scotton@epitech.eu>
2024-12-12 17:15:31 +0100

    add: adding languages regexp to the filename parser packages

--- packages/utilities/filename-parser/src/index.ts
+++ packages/utilities/filename-parser/src/index.ts
@@ -2,3 +2,4 @@
 export type { AudioChannel, AudioCodec } from './audio';
 export type { VideoResolution, VideoCodec } from './video';
 export type { ProviderSource } from './provider';
+export type { Language } from './language';
