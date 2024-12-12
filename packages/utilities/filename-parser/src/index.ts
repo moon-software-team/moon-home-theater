6368a568eeeb9b71c8a0fdbf03f692f8f2b5e377
mallory-scotton <mallory.scotton@epitech.eu>
2024-12-12 23:32:38 +0100

    add: adding export forwarding for newer functions

--- packages/utilities/filename-parser/src/index.ts
+++ packages/utilities/filename-parser/src/index.ts
@@ -3,3 +3,7 @@ export type { AudioChannel, AudioCodec } from './audio';
 export type { VideoResolution, VideoCodec } from './video';
 export type { ProviderSource } from './provider';
 export type { Language } from './language';
+
+/** Forward function */
+export { removeDoublon } from './utils';
+export { filenameParse } from './parse';
