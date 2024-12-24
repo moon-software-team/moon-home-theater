2900e8b90eaa7991379b943134a98a77529a1f0f
mallory-scotton <mallory.scotton@epitech.eu>
2024-12-24 10:08:14 +0100

    add: adding inner border utilities to the plugin

---
+++ packages/core/theme/src/plugin/inner-border.ts
@@ -0,0 +1,7 @@
+/** Export default box shadow */
+export const defaultBoxShadow = [
+  'var(--tw-ring-offset-shadow, 0 0 #0000)',
+  'var(--tw-ring-shadow, 0 0 #0000)',
+  'var(--tw-inner-border-shadow, 0 0 #0000)',
+  'var(--tw-shadow, 0 0 #0000)'
+].join(', ');
--- packages/core/theme/src/plugin/plugin.ts
+++ packages/core/theme/src/plugin/plugin.ts
@@ -7,11 +7,12 @@ import { typographies } from './typography';
 import { variants } from './variants';
 import { scrollbar } from './scrollbar';
 import { transitions } from './transition';
+import { defaultBoxShadow } from './inner-border';
 
 /** Create the Moon Tailwind plugin */
 export const MoonTwPlugin = () => {
   return plugin(
-    ({ addUtilities, addBase, addVariant }) => {
+    ({ addUtilities, addBase, addVariant, matchUtilities, theme, corePlugins }) => {
       /** Add the base colors based on the dark/light theme */
       addBase(addBaseColors());
 
@@ -23,6 +24,59 @@ export const MoonTwPlugin = () => {
 
       /** Add utilities for the transitions */
       addUtilities(transitions);
+
+      /** Adding base for inner border */
+      addBase({
+        'inner-border': {
+          '--tw-inner-border-color': theme('borderColor.DEFAULT', 'currentColor')
+        }
+      });
+
+      /** Adding utilities for inner border size */
+      matchUtilities(
+        {
+          'inner-border': (value) => ({
+            '@defaults inner-border': {},
+            '--tw-inner-border-width': value,
+            '--tw-inner-border-shadow':
+              'inset 0 0 0 var(--tw-inner-border-width) var(--tw-inner-border-color)',
+            'box-shadow': defaultBoxShadow
+          })
+        },
+        {
+          type: ['line-width', 'length'],
+          values: theme('borderWidth')
+        }
+      );
+
+      /** Adding utilities for inner border color */
+      matchUtilities(
+        {
+          'inner-border': (value) => ({
+            '--tw-inner-border-color': value
+          })
+        },
+        {
+          type: ['color', 'any'],
+          values: (({ DEFAULT: _, ...colors }) => colors)(theme('borderColor'))
+        }
+      );
+
+      /* Overrides to mitigate precedence issues */
+      if (corePlugins('boxShadow')) {
+        matchUtilities(
+          { shadow: () => ({ 'box-shadow': defaultBoxShadow }) },
+          { values: theme('boxShadow'), type: ['shadow'] }
+        );
+      }
+
+      /* Overrides to mitigate precedence issues */
+      if (corePlugins('ringWidth')) {
+        matchUtilities(
+          { ring: () => ({ 'box-shadow': defaultBoxShadow }) },
+          { values: theme('ringWidth'), type: 'length' }
+        );
+      }
 
       /** Add all the variants */
       for (const key of Object.keys(variants) as Array<keyof typeof variants>) {
